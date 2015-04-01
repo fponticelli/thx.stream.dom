60e6e157d10e11b9f06c7d57cfcdf853bbfe3b82
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:12:06 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f thx.stream.dom.zip
-zip -r thx.stream.dom.zip hxml src demo doc/ImportStreamDom.hx extraParams.hxml haxelib.json LICENSE README.md
+zip -r thx.stream.dom.zip hxml src demo doc/ImportStreamDom.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.stream.dom.zip
