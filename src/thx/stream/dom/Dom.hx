13fbdf40e4de0dfeea7e30430a859d0ce8127ea9
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-10-12 22:14:18 -0600

    updated to latest thx.stream

--- src/thx/stream/dom/Dom.hx
+++ src/thx/stream/dom/Dom.hx
@@ -7,7 +7,8 @@ import js.html.KeyboardEvent;
 import js.html.InputElement;
 import js.html.Event;
 using thx.Nil;
-using thx.stream.Emitter;
+using thx.stream.Stream;
+using thx.stream.StreamExtensions;
 using thx.promise.Promise;
 
 class Dom {
@@ -18,71 +19,70 @@ class Dom {
       }, false);
     });
 
-  public inline static function streamClick(el : Element, capture = false) : Emitter<MouseEvent>
+  public inline static function streamClick(el : Element, capture = false) : Stream<MouseEvent>
     return streamMouseEvent(el, 'click', capture);
 
-  public static function streamEvent<TEvent : Event>(el : Element, name : String, capture = false) : Emitter<TEvent>
-    return new Emitter(function(stream) {
-      el.addEventListener(name, stream.pulse, capture);
-      stream.addCleanUp(function() el.removeEventListener(name, stream.pulse, capture));
+  public static function streamEvent<TEvent : Event>(el : Element, name : String, capture = false) : Stream<TEvent>
+    return Stream.cancellable(function(o, addCancel) {
+      el.addEventListener(name, o.next, capture);
+      addCancel(function() el.removeEventListener(name, o.next, capture));
     });
 
-  public static function streamFocus(el : Element, capture = false) : Emitter<Bool>
+  public static function streamFocus(el : Element, capture = false) : Stream<Bool>
     return streamEvent(el, 'focus', capture).toTrue().merge(streamEvent(el, 'blur', capture).toFalse());
 
-  public static function streamKey(el : Element, name : String, capture = false) : Emitter<KeyboardEvent>
-    return new Emitter({
-      if(!name.startsWith('key'))
-        name = 'key$name';
-      function(stream) {
-        el.addEventListener(name, stream.pulse, capture);
-        stream.addCleanUp(function() el.removeEventListener(name, stream.pulse, capture));
-      }
+  public static function streamKey(el : Element, name : String, capture = false) : Stream<KeyboardEvent> {
+    if(!name.startsWith('key'))
+      name = 'key$name';
+    return Stream.cancellable(function(stream, addCancel) {
+      el.addEventListener(name, stream.next, capture);
+      addCancel(function() el.removeEventListener(name, stream.next, capture));
     });
+  }
 
-  public inline static function streamChecked(el : InputElement, capture = false) : Emitter<Bool>
+  public inline static function streamChecked(el : InputElement, capture = false) : Stream<Bool>
     return streamEvent(el, 'change', capture).map(function(_) return el.checked);
 
-  public inline static function streamChange(el : InputElement, capture = false) : Emitter<String>
+  public inline static function streamChange(el : InputElement, capture = false) : Stream<String>
     return streamEvent(el, 'change', capture).map(function(_) return el.value);
 
-  public inline static function streamInput(el : InputElement, capture = false) : Emitter<String>
+  public inline static function streamInput(el : InputElement, capture = false) : Stream<String>
     return streamMouseEvent(el, 'input', capture).map(function(_) return el.value);
 
-  public inline static function streamMouseDown(el : Element, capture = false) : Emitter<MouseEvent>
+  public inline static function streamMouseDown(el : Element, capture = false) : Stream<MouseEvent>
     return streamEvent(el, "mousedown", capture);
 
-  public inline static function streamMouseEvent(el : Element, name : String, capture = false) : Emitter<MouseEvent>
+  public inline static function streamMouseEvent(el : Element, name : String, capture = false) : Stream<MouseEvent>
     return streamEvent(el, name, capture);
 
-  public inline static function streamMouseMove(el : Element, capture = false) : Emitter<MouseEvent>
+  public inline static function streamMouseMove(el : Element, capture = false) : Stream<MouseEvent>
     return streamEvent(el, "mousemove", capture);
 
-  public inline static function streamMouseUp(el : Element, capture = false) : Emitter<MouseEvent>
+  public inline static function streamMouseUp(el : Element, capture = false) : Stream<MouseEvent>
     return streamEvent(el, "mouseup", capture);
 
-  public static function subscribeAttribute<T>(el : Element, name : String) : T -> Void
+  public static function setAttribute<T>(el : Element, name : String) : T -> Void
     return function(value : T) if(null == value) el.removeAttribute(name) else el.setAttribute(name, cast value);
 
-  public static function subscribeFocus(el : Element) : Bool -> Void
+  public static function setFocus(el : Element) : Bool -> Void
     return function(focus : Bool) if(focus) el.focus() else el.blur();
 
-  public static function subscribeHTML(el : Element) : String -> Void
+  public static function setHTML(el : Element) : String -> Void
     return function(html : String) el.innerHTML = html;
 
-  public static function subscribeText(el : Element, force = false) : String -> Void
+  public static function setText(el : Element, force = false) : String -> Void
     return function(text : String) if(el.textContent != text || force) el.textContent = text;
 
-  public static function subscribeToggleAttribute<T>(el : Element, name : String, ?value : T) : Bool -> Void {
+  public static function setToggleAttribute<T>(el : Element, name : String, ?value : T) : Bool -> Void {
     if(null == value)
       value = cast el.getAttribute(name);
     return function(on) if(on) el.setAttribute(name, cast value) else el.removeAttribute(name);
   }
 
-  public static function subscribeToggleClass(el : Element, name : String) : Bool -> Void
+  public static function setToggleClass(el : Element, name : String) : Bool -> Void
     return function(on) if(on) el.classList.add(name) else el.classList.remove(name);
 
-  public static function subscribeSwapClass(el : Element, nameOn : String, nameOff : String) : Bool -> Void
+  public static function setSwapClass(el : Element, nameOn : String, nameOff : String) : Bool -> Void
     return function(on) if(on) {
         el.classList.add(nameOn);
         el.classList.remove(nameOff);
@@ -91,7 +91,7 @@ class Dom {
         el.classList.remove(nameOn);
       };
 
-  public static function subscribeToggleVisibility(el : Element) : Bool -> Void {
+  public static function setToggleVisibility(el : Element) : Bool -> Void {
     var originalDisplay = el.style.display;
     if(originalDisplay == 'none')
       originalDisplay = '';
